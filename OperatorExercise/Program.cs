c4a2e88faba48f8874bb05e130e1d7cf0c5f768f
Shrividya Gopi <shrividya.gopi@gmail.com>
2024-10-14 19:22:19 -0400

    Excerise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int difference = a - b;
+            int divide = a / b;
+            int product  = a * b;
+            int modbus = a % b;
+            Console.WriteLine($"{a}/{b} is = {divide} remainder {modbus}.");
         }
     }
 }
