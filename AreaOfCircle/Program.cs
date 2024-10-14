fdb036fdbcfd88c81cceb1b8239a7d3c25de25f2
Shrividya Gopi <shrividya.gopi@gmail.com>
2024-10-14 19:59:09 -0400

    Excerise 2

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,15 @@
+﻿
+class Program
+{
+    static double AreaOfCircle1(double radius)
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Enter the radius of the circle:");
+        double radius = double.Parse(Console.ReadLine());
+        double area = AreaOfCircle1(radius); 
+        Console.WriteLine($"The area of the circle with radius {radius} is {area}");
+    }
+    }
